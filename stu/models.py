b66ffaeb71476512793a7ba0263477910a37dac2
ameyavedpathak <ameya.vedpathak@gmail.com>
2018-05-23 19:51:14 -0500

    first commit

---
+++ stu/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Student (models.Model):
+	Name=models.CharField(max_length=20)
+	Roll_no=models.CharField(max_length=20)
+	Father_name=models.CharField(max_length=20)
+	Address=models.TextField()
+
+
+
